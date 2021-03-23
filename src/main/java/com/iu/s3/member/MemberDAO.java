cd39cea2e60588b150452803c32a86eb01c523ea
rihee1214 <rihee1214@gmail.com>
2021-03-23 14:34:46 +0900

    member반완성

---
+++ src/main/java/com/iu/s3/member/MemberDAO.java
@@ -0,0 +1,37 @@
+package com.iu.s3.member;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	private final String NAMESPACE="com.iu.s3.member.MemberDAO.";
+	
+	public int memberDelete(MemberDTO memberDTO) throws Exception{
+		return sqlSession.delete(NAMESPACE+"memberDelete", memberDTO);
+	}
+	
+	public int memberUpdate(MemberDTO memberDTO) throws Exception{
+		return sqlSession.update(NAMESPACE+"memberUpdate", memberDTO);
+	}
+	
+	//memberJoin 데이터를 받아서 DB에 insert 하는 메서드
+	public int memberJoin(MemberDTO memberDTO)throws Exception{
+		return sqlSession.insert(NAMESPACE+"memberJoin", memberDTO);
+	}
+	
+	//login - id pw를 받아서 조회
+	public MemberDTO login(MemberDTO memberDTO)throws Exception{
+		return sqlSession.selectOne(NAMESPACE+"memberLogin", memberDTO);
+	}
+
+}
---
+++ src/test/java/com/iu/s3/member/MemberDAOTest.java
@@ -0,0 +1,22 @@
+package com.iu.s3.member;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.iu.s3.MyAbstractTest;
+
+public class MemberDAOTest extends MyAbstractTest {
+
+	@Autowired
+	private MemberDAO memberDAO;
+	
+	@Test
+	public void memberLoginTest() throws Exception{
+		MemberDTO memberDTO = new MemberDTO();
+		memberDTO.setId("id1");
+		memberDTO.setPw("pw1");
+	}
+
+}
